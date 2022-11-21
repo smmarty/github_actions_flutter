c14556e0ab58130f5f0a0f18c73847d96bb48030
Nikita Spirianov <sna2021@narod.ru>
2022-11-21 13:01:48 +0300

    ios fix build mode case

--- .github/scripts/ios/assemble_ios.sh
+++ .github/scripts/ios/assemble_ios.sh
@@ -6,6 +6,7 @@ then
  result_target=lib/targets/${BUILD_FLAVOR}.dart
 fi
 
-echo -n "Build flavor:"$BUILD_FLAVOR" target $result_target mode $BUILD_MODE"
-#flutter build ios -t $result_target --$BUILD_MODE --no-codesign --flavor $BUILD_FLAVOR
-flutter build ios -t $result_target --$BUILD_MODE --no-codesign
+mode="$(tr -s "[:upper:]" "[:lower:]" <<<$BUILD_MODE)"
+
+echo -n "Build flavor:"$BUILD_FLAVOR" target $result_target mode $mode"
+flutter build ios -t $result_target --$mode --no-codesign --flavor $BUILD_FLAVOR

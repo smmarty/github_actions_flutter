ac6a0af05facd2044f0b1886df777f0495f83aa1
Nikita Spirianov <sna2021@narod.ru>
2023-12-16 18:20:19 +0300

    fix: update share_filename

--- actions/minio/entrypoint.sh
+++ actions/minio/entrypoint.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 mc --version
 mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
 
-mc cp $1 "deploy/$2"
+mc cp $1 "deploy/$2/$3"
 
 echo "link=${mc download "deploy/$2/$3"}" >> $GITHUB_OUTPUT

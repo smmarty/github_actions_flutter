927da8b105be3ab661bc0061333a5472a604c5c0
Nikita Spirianov <sna2021@narod.ru>
2023-12-17 17:48:53 +0300

    format with jq

--- actions/minio/entrypoint.sh
+++ actions/minio/entrypoint.sh
@@ -5,4 +5,4 @@ mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3
 
 mc cp $1 "deploy/$2/$3"
 
-echo "link=$(mc share download "deploy/$2/$3" | grep -o 'Share: .*' | grep -o 'http.*')" >> $GITHUB_OUTPUT
+echo "link=$(mc share download "deploy/$2/$3" | grep -o 'Share: .*' | grep -o 'http.*' | jq --slurp --raw-input --raw-output @uri)" >> $GITHUB_OUTPUT

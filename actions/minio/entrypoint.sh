50b786511a2adfd86f64f41e808d35dcd817be64
Nikita Spirianov <sna2021@narod.ru>
2023-12-18 15:35:02 +0300

    fix: update source file escaping

--- actions/minio/entrypoint.sh
+++ actions/minio/entrypoint.sh
@@ -3,6 +3,6 @@
 mc --version
 mc alias set deploy $MINIO_ENDPOINT $MINIO_ACCESS_KEY $MINIO_SECRET_KEY --api S3v4
 
-mc cp $1 "deploy/$2/$3"
+mc cp "$1" "deploy/$2/$3"
 
 echo "link=$(mc share download "deploy/$2/$3" | grep -o 'Share: .*' | grep -o 'http.*' | jq --slurp --raw-input --raw-output @uri)" >> $GITHUB_OUTPUT
